6f2b6c9411708ebc0e9c82fa70acf423d93c7f5e
Danilzey <88266028+Ahmadskylord@users.noreply.github.com>
2022-10-04 08:10:19 -0700

    Add files via upload
    tipe data menngunakan c++

---
+++ tipedata.ccp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+//hii perkenalkan nama saya ahmad danial hariadi saat ini saya ingin belajar type data menggunakan bahasa pemrograman c++
+int main(){
+  //tipe data
+  /*
+    string = menuliskan kaliamat
+    interger = bilangan bulat
+  floating point = bilangan pecahan
+doble dloatig point = bilangan pecahan
+  char = character terbatas -1 hurup
+  bool - konsisi true or false
+*/
+  string namasaya = "danilzey";
+  int nomor_int =10;
+  float nomor_float =10.1;
+  double nomor_double =10.2;
+  char karakter ='A';
+  bool bolean = true;
+
+  cout<<"string namasaya : "<<namasaya<<endl;
+  cout<<"nomor int : "<<nomor_int<<endl;
+  cout<<"nomor_float :"<<nomor_float;
+  cout<<"nomor_double:"<<nomor_double;
+  cout<<"char:"<<karakter;
+  cout<<"bolean:"<<bolean;
+}
